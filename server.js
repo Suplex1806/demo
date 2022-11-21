af04596eaf2a37128180da950808d42cbaa26ec5
Suplex1806 <118749697+Suplex1806@users.noreply.github.com>
2022-11-21 21:14:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl  ,(err) => {
+mongoose.connect(dbUrl,{useUnifiedTopology: true }  ,(err) => {
   console.log('mongodb connected',err);
 })
 
