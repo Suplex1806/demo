4400a1e91a93c9e652c7b9f30310f159426e0ff2
Suplex1806 <118749697+Suplex1806@users.noreply.github.com>
2022-11-21 21:09:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+mongoose.connect(dbUrl  ,(err) => {
   console.log('mongodb connected',err);
 })
 
